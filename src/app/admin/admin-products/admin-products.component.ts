19e4778ac5ab9d4ae3eaa1aa57bcdd090ab220c5
unknown <bjoslishubho@gmail.com>
2020-08-13 19:44:46 +0600

    updated admin products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,17 +1,33 @@
+import { Product } from './../../models/products';
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: Product[];
+  filterdProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) { 
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService.getAll()
+      .subscribe((products:any) => this.filterdProducts = this.products = products);
+  }
+
+  filter(query: string) {
+    console.log(query);
+    this.filterdProducts = (query) ?
+      this.products.filter(p => p.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())) : 
+      this.products;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   ngOnInit(): void {
