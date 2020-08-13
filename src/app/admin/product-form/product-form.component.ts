64ce313b865841e46735161459ed967e7f3166fb
unknown <bjoslishubho@gmail.com>
2020-08-13 17:07:31 +0600

    product update and delete option added

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -9,9 +9,10 @@ import { take } from 'rxjs/operators';
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.css']
 })
-export class ProductFormComponent implements OnInit {
+export class ProductFormComponent {
   categories$;
   product;
+  id;
 
   constructor(
     private router: Router,
@@ -21,19 +22,29 @@ export class ProductFormComponent implements OnInit {
 
     this.categories$ = categoryService.getCategories();
 
-    let id = this.route.snapshot.paramMap.get('id');
-    if (id) {
-      this.productService.get(id).pipe(take(1)).subscribe(p => this.product = p);
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) {
+      // this.productService.get(this.id).pipe(take(1)).subscribe(p => this.product = p);
+      this.productService.get(this.id).subscribe(p => this.product = p);
     }
 
   }
 
   save(product) {
-    this.productService.create(product);
+    if (this.id)
+      this.productService.update(this.id, product);
+    else
+      this.productService.create(product);
+
     this.router.navigate(['/admin/products']);
   }
 
-  ngOnInit(): void {
+  delete() {
+    if (!confirm('Are you sure you want to delete this product?'))
+      return;
+
+    this.productService.delete(this.id);
+    this.router.navigate(['/admin/products']);
   }
 
 }
