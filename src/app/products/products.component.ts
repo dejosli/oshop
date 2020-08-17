0b4e87031e367b7bb5d2ee7270cf5199c410ff10
unknown <bjoslishubho@gmail.com>
2020-08-17 23:52:01 +0600

    filtered according to category

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,7 +1,9 @@
+import { CategoryService } from './../category.service';
 import { ProductService } from './../product.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Product } from '../models/products';
 import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -12,13 +14,28 @@ import { Subscription } from 'rxjs';
 export class ProductsComponent implements OnDestroy {
   // products$;
   products: Product[];
+  filteredProducts: Product[];
   subscription: Subscription;
+  categories$;
+  category: string;
 
-  constructor(private productService: ProductService) {
+  constructor(
+    route: ActivatedRoute,
+    private productService: ProductService, 
+    private categoryService: CategoryService) {
     // this.products$ = productService.getAll();
     this.subscription = this.productService.getAll().subscribe(products => {
       this.products = products;
       console.log(products);
+    });
+    this.categories$ = categoryService.getCategories();
+
+    route.queryParamMap.subscribe(params => {
+      this.category = params.get('category');
+
+      this.filteredProducts = (this.category) ? 
+        this.products.filter(p => p.category === this.category) :
+        this.products;
     });
    }
 
