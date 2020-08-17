5ad2805987dcd09bf4868ef41e33d94485977b9f
unknown <bjoslishubho@gmail.com>
2020-08-17 13:02:58 +0600

    search option added

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,17 +1,33 @@
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: any[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) { 
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService.getAll().subscribe(product =>
+      this.filteredProducts = this.products = product);
+  }
+
+  filter(query: string) {
+    // console.log(query);
+    // this.filteredProducts = (query) ?
+    //   this.products.filter(p => 
+    //     p.title.includes(query));
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+
   }
 
   ngOnInit(): void {
